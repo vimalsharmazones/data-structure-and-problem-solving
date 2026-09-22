ae7e096db26b4503ff2abb8c42a8044d1acee165
vimalsharmazones <63034391+vimalsharmazones@users.noreply.github.com>
2026-09-22 12:13:35 +0530

    PalindromeNumber

---
+++ LeetCodeInJava/LeetCode09PalindromeNumber.java
@@ -0,0 +1,22 @@
+public class LeetCode09PalindromeNumber {
+    public boolean isPalindrome(int x) {
+
+        // Negative numbers are not palindromes due to the '-' sign.
+        // If Last digit is 0, that it should be first digit also.
+
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+        int orginalX = x;
+
+        while (x > 0) {
+            int remainder = x % 10;
+            reversed = (reversed * 10) + remainder;
+            x /= 10;
+        }
+
+        return reversed == orginalX;
+    }
+}
